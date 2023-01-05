0cc45f6ed8215deb76738748a5320c9587449e73
gemi254 <gemi254@192.168.56.1>
2023-01-05 22:57:28 +0200

    Send motion start/stop socket messages

--- motionDetect.cpp
+++ motionDetect.cpp
@@ -136,6 +136,9 @@ bool checkMotion(camera_fb_t* fb, bool motionStatus) {
     if (!motionStatus && motionCnt >= detectMotionFrames) {
       LOG_DBG("***** Motion - START");
       motionStatus = true; // motion started
+#ifdef USE_WEBSOCKET_SERVER
+      socketSendToServer("MotionStart");
+#endif
     } 
     if (dbgMotion)
       // to highlight movement detected in changeMap image, set all gray in region of interest to black
@@ -147,6 +150,9 @@ bool checkMotion(camera_fb_t* fb, bool motionStatus) {
       LOG_DBG("***** Motion - STOP after %u frames", motionCnt);
       motionCnt = 0;
       motionStatus = false; // motion stopped
+#ifdef USE_WEBSOCKET_SERVER
+      socketSendToServer("MotionEnd");
+#endif
     }
   }
   if (motionStatus) LOG_DBG("*** Motion - ongoing %u frames", motionCnt);
